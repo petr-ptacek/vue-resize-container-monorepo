60034c9290e9c2550ec0b33789d9f8026d114090
Petr Ptacek <petr.ptacek@olc.cz>
2025-07-10 10:05:33 +0200

    update

--- packages/vue-resize-container/vite.config.ts
+++ packages/vue-resize-container/vite.config.ts
@@ -1,56 +1,104 @@
+// https://vite.dev/config/
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 
 import vue from "@vitejs/plugin-vue";
-import { defineConfig } from "vite";
+import { defineConfig, type UserConfig } from "vite";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// https://vite.dev/config/
-export default defineConfig(({ command }) => {
-  const isBuild = command === "build";
-
+const createConfig = (): UserConfig => {
   return {
     plugins: [vue()],
-    build: isBuild
-      ? {
-          // cssCodeSplit: true,
-          lib: {
-            cssFileName: "vue-resize-container",
-            // entry: {
-            //   index: fileURLToPath(new URL("./src/index.ts", import.meta.url)),
-            //   style: fileURLToPath(new URL("./src/assets/styles/style.css", import.meta.url))
-            // },
-            entry: resolve(__dirname, "./src/index.ts"),
-            name: "VueResizeContainer",
-            formats: ["es", "umd"],
-            fileName: (format, entryName) => {
-              switch (entryName) {
-                case "style":
-                  return `style.css`;
-                default:
-                  return `vue-resize-container.${format}.js`;
-              }
-            },
-          },
-          rollupOptions: {
-            external: ["vue"],
-            output: {
-              globals: {
-                vue: "Vue",
-              },
-            },
-          },
-        }
-      : undefined,
-    resolve: {
-      alias: [
-        {
-          find: "vue-resize-container",
-          replacement: resolve("./src"),
+    build: {
+      // cssCodeSplit: true,
+      lib: {
+        cssFileName: "vue-resize-container",
+        // entry: {
+        //   index: fileURLToPath(new URL("./src/index.ts", import.meta.url)),
+        //   style: fileURLToPath(new URL("./src/assets/styles/style.css", import.meta.url))
+        // },
+        entry: resolve(__dirname, "./src/index.ts"),
+        name: "VueResizeContainer",
+        formats: ["es", "umd"],
+        fileName: (format, entryName) => {
+          switch (entryName) {
+            case "style":
+              return `style.css`;
+            default:
+              return `vue-resize-container.${format}.js`;
+          }
         },
-      ],
+      },
+      rollupOptions: {
+        external: ["vue"],
+        output: {
+          globals: {
+            vue: "Vue",
+          },
+        },
+      },
     },
-    root: isBuild ? undefined : "playground",
   };
+};
+
+export default defineConfig(({ command }) => {
+  const config: UserConfig = createConfig();
+
+  switch (command) {
+    case "build":
+      config.build = undefined;
+      break;
+  }
+
+  return config;
 });
+
+// https://vite.dev/config/
+// export default defineConfig(({ command }) => {
+//   const isBuild = command === "build";
+//
+//   return {
+//     plugins: [vue()],
+//     build: isBuild
+//            ? {
+//         // cssCodeSplit: true,
+//         lib: {
+//           cssFileName: "vue-resize-container",
+//           // entry: {
+//           //   index: fileURLToPath(new URL("./src/index.ts", import.meta.url)),
+//           //   style: fileURLToPath(new URL("./src/assets/styles/style.css", import.meta.url))
+//           // },
+//           entry: resolve(__dirname, "./src/index.ts"),
+//           name: "VueResizeContainer",
+//           formats: ["es", "umd"],
+//           fileName: (format, entryName) => {
+//             switch ( entryName ) {
+//               case "style":
+//                 return `style.css`;
+//               default:
+//                 return `vue-resize-container.${ format }.js`;
+//             }
+//           }
+//         },
+//         rollupOptions: {
+//           external: ["vue"],
+//           output: {
+//             globals: {
+//               vue: "Vue"
+//             }
+//           }
+//         }
+//       }
+//            : undefined,
+//     resolve: {
+//       alias: [
+//         {
+//           find: "vue-resize-container",
+//           replacement: resolve("./src")
+//         }
+//       ]
+//     },
+//     root: isBuild ? undefined : "playground"
+//   };
+// });
