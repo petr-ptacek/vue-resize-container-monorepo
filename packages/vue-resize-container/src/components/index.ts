2002a1209bea4b40b157ee93e70eefa65f23ee2a
Petr Ptacek <petr.ptacek@olc.cz>
2025-07-11 16:37:45 +0200

    update

--- packages/vue-resize-container/src/components/index.ts
+++ packages/vue-resize-container/src/components/index.ts
@@ -1,6 +1,6 @@
 import ButtonsBox from "./ButtonsBox.vue";
-import IconCollapse from "./IconCollapse.vue";
-import IconExpand from "./IconExpand.vue";
+import IconShevronLeft from "./IconShevronLeft.vue";
+import IconShevronRight from "./IconShevronRight.vue";
 import ResizeContainerSection from "./ResizeContainerSection.vue";
 
-export { ResizeContainerSection, ButtonsBox, IconExpand, IconCollapse };
+export { ResizeContainerSection, ButtonsBox, IconShevronRight, IconShevronLeft };
