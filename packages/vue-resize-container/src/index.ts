dc5d3d7cab87986cc8b0375a5ce2d5994cecab84
Petr Ptacek <petr.ptacek@olc.cz>
2025-07-14 10:29:18 +0200

    update

--- packages/vue-resize-container/src/index.ts
+++ packages/vue-resize-container/src/index.ts
@@ -1,11 +1,7 @@
 import("./assets/styles/main.css");
-import {
-  IconCollapse as VueIconCollapse,
-  IconExpand as VueIconExpand,
-  ResizeContainerSection as VueResizeContainerSection,
-} from "./components";
+import { IconShevronRight, IconShevronLeft, ResizeContainerSection as VueResizeContainerSection } from "./components";
 import VueResizeContainer from "./VueResizeContainer.vue";
 
 export * from "./types";
 
-export { VueResizeContainer, VueResizeContainerSection, VueIconExpand, VueIconCollapse };
+export { VueResizeContainer, VueResizeContainerSection, IconShevronLeft, IconShevronRight };
